201290d8bf9117922945a2c4a275415952057c65
Amanjot Singh <amanjot6363@gmail.com>
2023-05-31 09:16:01 +0530

    Project Updated with AS Flamingo

--- app/src/main/java/com/amansingh/playservicesinformation/MainActivity.kt
+++ app/src/main/java/com/amansingh/playservicesinformation/MainActivity.kt
@@ -1,65 +1,79 @@
 package com.amansingh.playservicesinformation
 
 import android.app.AlertDialog
-import android.content.DialogInterface
 import android.content.Intent
 import android.content.pm.PackageInfo
 import android.content.pm.PackageManager
 import android.net.Uri
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings
 import android.util.Log
 import android.view.View
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.appcompat.app.AppCompatActivity
+import com.amansingh.playservicesinformation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    val TAG: String = this.javaClass.simpleName
-    val PLAY_SERVICES_PACKAGE_NAME: String = "com.google.android.gms"
-    val PLAY_STORE_PACKAGE_NAME: String = "com.android.vending"
-    var packageInfo: PackageInfo? = null
+    private val _TAG: String = this.javaClass.simpleName
+    private val PLAY_SERVICES_PACKAGE_NAME: String = "com.google.android.gms"
+    private val PLAY_STORE_PACKAGE_NAME: String = "com.android.vending"
+    private var packageInfo: PackageInfo? = null
+    private lateinit var binding: ActivityMainBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         try {
             packageInfo = packageManager.getPackageInfo(PLAY_SERVICES_PACKAGE_NAME, 0)
-        } catch (exeption: PackageManager.NameNotFoundException) {
-            Log.e(TAG, "Package $PLAY_SERVICES_PACKAGE_NAME not found")
+        } catch (exception: PackageManager.NameNotFoundException) {
+            Log.e(_TAG, "Package $PLAY_SERVICES_PACKAGE_NAME not found")
         }
 
         setDataOnView(packageInfo)
 
-        tvOpenApkMirror.setOnClickListener {
-            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://www.apkmirror.com/apk/google-inc/google-play-services/")))
+        binding.tvOpenApkMirror.setOnClickListener {
+            startActivity(
+                Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("https://www.apkmirror.com/apk/google-inc/google-play-services/")
+                )
+            )
         }
 
-        tvOpenPlayStore.setOnClickListener {
+        binding.tvOpenPlayStore.setOnClickListener {
             var url: String?
             try {
                 packageManager.getPackageInfo(PLAY_STORE_PACKAGE_NAME, 0)
                 url = "market://details?id=$PLAY_SERVICES_PACKAGE_NAME"
-            } catch (execption: PackageManager.NameNotFoundException) {
-                Log.e(TAG, "Package $PLAY_STORE_PACKAGE_NAME not found")
+            } catch (exception: PackageManager.NameNotFoundException) {
+                Log.e(_TAG, "Package $PLAY_STORE_PACKAGE_NAME not found")
                 url = "https://play.google.com/store/apps/details?id=$PLAY_SERVICES_PACKAGE_NAME"
             }
             startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
         }
 
-        tvOpenAppInfo.setOnClickListener {
-            startActivity(Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, Uri.fromParts("package", PLAY_SERVICES_PACKAGE_NAME, null)))
+        binding.tvOpenAppInfo.setOnClickListener {
+            startActivity(
+                Intent(
+                    Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                    Uri.fromParts("package", PLAY_SERVICES_PACKAGE_NAME, null)
+                )
+            )
         }
 
-        tvAbout.setOnClickListener {
+        binding.tvAbout.setOnClickListener {
             AlertDialog.Builder(this)
-                    .setTitle("About")
-                    .setMessage("Play Services Info (${BuildConfig.VERSION_NAME})\n\nby Amanjot Singh\namansingh63@xda.com")
-                    .setPositiveButton("Close", DialogInterface.OnClickListener { dialogInterface, i -> })
-                    .create()
-                    .show()
+                .setTitle("About")
+                .setMessage("Play Services Info (${BuildConfig.VERSION_NAME})\n\nby Amanjot Singh\namansingh63@xda.com")
+                .setPositiveButton(
+                    "Close"
+                ) { _, _ -> }
+                .create()
+                .show()
         }
 
     }
@@ -67,17 +81,18 @@ class MainActivity : AppCompatActivity() {
     private fun setDataOnView(packageInfo: PackageInfo?) {
 
         if (applicationInfo != null && packageInfo != null) {
-            ivIcon.setImageResource(R.mipmap.ic_play_services)
-            tvInstallStatus.text = getString(R.string.text_play_services_installed)
-            tvPlayServicesVersion.text = "Version Installed : ${packageInfo?.versionName}"
-            tvPlayServicesVarient.text = "Variant : ${packageInfo?.versionName.substring(9, 12)}"
+            binding.ivIcon.setImageResource(R.mipmap.ic_play_services)
+            binding.tvInstallStatus.text = getString(R.string.text_play_services_installed)
+            binding.tvPlayServicesVersion.text = "Version Installed : ${packageInfo.versionName}"
+            binding.tvPlayServicesVarient.text =
+                "Variant : ${packageInfo.versionName?.substring(10, 16)}"
         } else {
-            ivIcon.setImageResource(R.mipmap.ic_play_services_greysclae)
-            tvInstallStatus.text = getString(R.string.text_play_services_installed)
-            tvPlayServicesVersion.visibility = View.GONE
-            tvPlayServicesVarient.visibility = View.GONE
-            divider1.visibility = View.GONE
-            tvOpenAppInfo.visibility = View.GONE
+            binding.ivIcon.setImageResource(R.mipmap.ic_play_services_greysclae)
+            binding.tvInstallStatus.text = getString(R.string.text_play_services_installed)
+            binding.tvPlayServicesVersion.visibility = View.GONE
+            binding.tvPlayServicesVarient.visibility = View.GONE
+            binding.divider1.visibility = View.GONE
+            binding.tvOpenAppInfo.visibility = View.GONE
 
         }
     }
